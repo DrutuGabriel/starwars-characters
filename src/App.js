663e31182207242c52415a60ec24a973f3735da2
Gabriel D <gabriel.drutu@gmail.com>
2020-03-04 12:07:24 +0200

    Add access control origin header to fetch

--- src/App.js
+++ src/App.js
@@ -15,9 +15,10 @@ class App extends Component {
 	}
 
 	componentDidMount(){
-		fetch('https://jsonplaceholder.typicode.com/users', 
-		{
-			mode: 'no-cors',
+		fetch('https://jsonplaceholder.typicode.com/users', {
+			headers: {
+				'Access-Control-Allow-Origin' : 'https://jsonplaceholder.typicode.com'
+			}
 		})
 		.then(response => response.json())
 		.then(users => this.setState({ monsters: users }));
