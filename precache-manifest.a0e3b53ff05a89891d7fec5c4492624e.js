f5b2c88ac88981fdef5d77dc7f8b93f7615d9b0f
Gabriel D <gabriel.drutu@gmail.com>
2021-02-06 16:36:01 +0200

    Updates

---
+++ precache-manifest.a0e3b53ff05a89891d7fec5c4492624e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fac82f0cb0d4dca2bd77d3659aa0419",
+    "url": "/starwars-characters/index.html"
+  },
+  {
+    "revision": "9ce95e3e248a1d3119e9",
+    "url": "/starwars-characters/static/css/main.3990f3f2.chunk.css"
+  },
+  {
+    "revision": "9e2d2eb922c34b35186a",
+    "url": "/starwars-characters/static/js/2.f4ea2df8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/starwars-characters/static/js/2.f4ea2df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce95e3e248a1d3119e9",
+    "url": "/starwars-characters/static/js/main.efe2fb6e.chunk.js"
+  },
+  {
+    "revision": "081af0eab48f6bd89585",
+    "url": "/starwars-characters/static/js/runtime-main.2f05b040.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwars-characters/static/media/Starjedi.cc3019aa.ttf"
+  }
+]);
